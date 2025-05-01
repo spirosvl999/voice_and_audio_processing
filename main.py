b0a3ecccffc8266d46863bd3fda959eb4fc20da1
Vlachos Spiridon <79493906+spirosvl999@users.noreply.github.com>
2025-05-01 16:30:33 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.extract_features import create_dataset
+from models.least_squares_classifier import train_least_squares
+from models.mlp_classifier import train_mlp
+from src.predict_and_generate_csv import predict_and_export_csv
+
+if __name__ == "__main__":
+    print("[1] Extracting features...")
+    create_dataset("data/train/speech", "data/train/noise")
+
+    print("[2] Training Least Squares model...")
+    train_least_squares()
+
+    print("[3] Training MLP model...")
+    train_mlp()
+
+    print("[4] Predicting and generating CSV...")
+    predict_and_export_csv("data/test/file1.wav", model_type="mlp")

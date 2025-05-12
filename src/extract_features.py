7729667d15275cd902cbbd398b49190f1663f591
spirosvl999 <spirosvlachos.skr@outlook.com.gr>
2025-05-12 14:02:27 +0300

    Update extract_features.py

--- src/extract_features.py
+++ src/extract_features.py
@@ -29,11 +29,11 @@ def extract_features_from_directory(directory, label, sr=16000, frame_length=0.0
 
 def create_dataset(speech_dir, noise_dir):
     print("[Dataset] Loading speech files...")
-    speech_data = extract_features_from_directory(speech_dir, 'foreground')
+    speech_data = extract_features_from_directory(speech_dir, 'foreground')                                     # We use the extract_features_from_directory function for foreground.
     print(f"[Dataset] Loaded {len(speech_data)} speech samples.")
 
     print("[Dataset] Loading noise files...")
-    noise_data = extract_features_from_directory(noise_dir, 'background')
+    noise_data = extract_features_from_directory(noise_dir, 'background')                                       # We use the extract_features_from_directory function for background.
     print(f"[Dataset] Loaded {len(noise_data)} noise samples.")
 
     all_data = speech_data + noise_data
@@ -48,6 +48,6 @@ def create_dataset(speech_dir, noise_dir):
     labels = np.array([x[1] for x in all_data])
     filenames = np.array([x[2] for x in all_data])
 
-    os.makedirs("data", exist_ok=True)  # ensure directory exists
+    os.makedirs("data", exist_ok=True)                                                                                  # Εnsure directory exists.
     np.savez("data/features_dataset.npz", X=features, y=labels, filenames=filenames)
     print("[Dataset] Dataset saved to data/features_dataset.npz")
